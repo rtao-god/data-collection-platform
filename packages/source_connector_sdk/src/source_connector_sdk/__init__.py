ca721c068ca7c8adae425d007f79cc2985a1dbd5
rtao-god <rtaogod@gmail.com>
2026-08-12 19:35:37 +0400

    Feat (Connector SDK): expose strict Worker Gateway boundary

---
+++ packages/source_connector_sdk/src/source_connector_sdk/__init__.py
@@ -0,0 +1,19 @@
+from source_connector_sdk.gateway import (
+    ArtifactRead,
+    LeaseArtifact,
+    PreparedUpload,
+    SourceWorkerGateway,
+    VerifiedUpload,
+    WorkLease,
+    WorkerGatewayFailure,
+)
+
+__all__ = [
+    "ArtifactRead",
+    "LeaseArtifact",
+    "PreparedUpload",
+    "SourceWorkerGateway",
+    "VerifiedUpload",
+    "WorkLease",
+    "WorkerGatewayFailure",
+]
